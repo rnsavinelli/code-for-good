717252ed70601facc1669634a54129a8c83e2a50
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-11-12 19:34:21 -0300

    19:34

--- server/database.py
+++ server/database.py
@@ -41,6 +41,65 @@ def get_table(table):
 
     return data, columns
 
+def get_user_tags(tid):
+    conn, data, columns = None, None, None
+
+    with open("DATABASE.md","r") as file:
+        db = file.read().strip()
+
+    try:
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        c.execute(f'SELECT * FROM user_tag WHERE user_id="{tid}"')
+        data = c.fetchall()
+        columns = list(map(lambda x: x[0], c.description))
+        conn.commit()
+
+    except Error as error:
+        traceback.print_exc()
+        print(error)
+
+    finally:
+        if conn:
+            conn.close()
+
+    return data, columns
+
+
+def get_tag_names(tag_ids):
+    conn, data, columns = None, None, None
+
+    with open("DATABASE.md","r") as file:
+        db = file.read().strip()
+
+    try:
+        is_first = True
+        condition = str()
+        for tid in list(tag_ids):
+            if is_first:
+                condition = condition + "id IN (\"" + str(tid) + "\" "
+                is_first = False
+            else:
+                condition = condition + ",\"" + str(tid) + "\" "
+        condition = condition + ")"                
+
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        c.execute(f'SELECT * FROM tag WHERE {condition}')
+        data = c.fetchall()
+        columns = list(map(lambda x: x[0], c.description))
+        conn.commit()
+
+    except Error as error:
+        traceback.print_exc()
+        print(error)
+
+    finally:
+        if conn:
+            conn.close()
+
+    return data, columns
+
 def get_user_by_credentials(table, username, password):
     conn, data, columns = None, None, None
 
@@ -82,15 +141,31 @@ def write_table(table, dic):
     for key in dic.keys():
         if is_first:
             columns = columns + "\"" + key + "\""
-            values = values + "\"" + dic[key] + "\""
+            values = values + "\"" + str(dic[key]) + "\""
             is_first = False
         else:
             columns = columns + ",\"" + key + "\""
-            values = values + ",\"" + dic[key] + "\""
+            values = values + ",\"" + str(dic[key]) + "\""
 
     try:
         db_exec(db, f'INSERT INTO {table} ({columns}) VALUES ({values})')
-        return 0
+
+        is_first = True
+        condition = str()
+        for key in dic.keys():
+            if is_first:
+                condition = condition + key + "=\"" + str(dic[key]) + "\" "
+                is_first = False
+            else:
+                condition = condition + "AND " + key + "=\"" + str(dic[key]) + "\" "
+
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        c.execute(f'SELECT * FROM {table} WHERE {condition}')
+        data = c.fetchone()
+        columns = list(map(lambda x: x[0], c.description))
+        conn.commit()        
+        return data[0]
 
     except Error as e:
         traceback.print_exc()
