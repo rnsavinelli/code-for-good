2053f6dc1d7dc04eb0f662e7152cc48b16257ab2
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-11-12 16:49:13 -0300

    test: add routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Landing from "./pages/Landing";
+import Navbar from "./components/Navbar/Navbar";
+import Test from "./pages/Test";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </Router>
   );
