33a463ca88a78cce7ffe192769f728e19c24cd6a
florencia-belmana <florbel_03@hotmai.com>
2021-11-12 17:02:42 -0300

    card

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Landing from "./pages/Landing";
+import Login from "./pages/login";
+import Card from "./pages/Card";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Card" element={<Card />} />
       </Routes>
     </Router>
   );
