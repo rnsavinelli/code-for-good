d9db453bace7e73e653a67e63e86cc7135afcf76
florencia-belmana <florbel_03@hotmai.com>
2021-11-12 17:53:59 -0300

    stylecard

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
+ import Container from "@mui/material/Container"; 
 import { Helmet } from "react-helmet"; 
 
 function Login () {
     return (
-        <div>
+        <>
              <Helmet title="Ingresar" />
-              <h3> Ingresa acá! </h3>
-              <label for="email"><i class="fas fa-user "></i> Email:</label>
-              <input type="text" name="email" id="email" placeholder="juan@ejemplo.com"/>
-              <label for="password"><i class=" fas fa-key "></i> Password:</label>
-              <input type="password" name="password" id="password" placeholder=""/>
+             <Container>
+              <div className="mx-auto mt-5">
+                <h3> Ingresa acá! </h3>
+                <label for="email"><i class="fas fa-user "></i> Email:</label><br />
+                <input type="text" name="email" id="email" placeholder="juan@ejemplo.com"/><br/>
+                <label for="password"><i class=" fas fa-key "></i> Password:</label><br/>
+                <input type="password" name="password" id="password" placeholder=""/><br/>
          
-              <input type="submit" value="Ingresa" />
+                <input type="submit" value="Ingresa" />
                 <label for="">Ingresar</label>
-              <input type="checkbox" name="recordarme" />
+                <input type="checkbox" name="recordarme" />
                 <label for="">Recordarme</label>
-        </div>
-
-        )
+            </div>
+            </Container>
+        </>
+       
+      
+         
+        );
     
 }
  
