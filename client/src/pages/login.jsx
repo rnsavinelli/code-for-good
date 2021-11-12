0a790e88506e4cd4064d7dfafcfc154554028912
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-11-12 19:03:32 -0300

    feat: logins

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,33 +1,117 @@
-import React from 'react';
- import Container from "@mui/material/Container"; 
-import { Helmet } from "react-helmet"; 
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Helmet } from "react-helmet";
 
-function Login () {
-    return (
-        <>
-             <Helmet title="Ingresar" />
-             <Container>
-              <div className="mx-auto mt-5">
-                  
-                <h3> Ingresa acá! </h3>
-                <label for="email"><i class="fas fa-user "></i> Email:</label><br />
-                <input type="text" name="email" id="email" placeholder="juan@ejemplo.com"/><br/>
-                <label for="password"><i class=" fas fa-key "></i> Password:</label><br/>
-                <input type="password" name="password" id="password" placeholder=""/><br/>
-         
-                <input type="submit" value="Ingresa" />
-                <label for=""></label><br />
-                <input type="checkbox" name="recordarme" />
-                <label for="">Recordarme</label>
-            </div>
-            </Container>
-        </>
-       
-      
-         
-        );
-    
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
- 
-export default Login;
 
+const theme = createTheme();
+
+export default function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Helmet title="Iniciar Sesion" />
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Iniciar Sesión
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Contraseña"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Recuerdame me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Iniciar Sesión
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Olvido su contraseña?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"No tengo cuenta? Registrame"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
