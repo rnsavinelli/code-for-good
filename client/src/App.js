19041ac3bd86a2b101ba7060705967e267b9167c
lschirripa <lschirripa@frba.utn.edu.ar>
2021-11-12 16:50:36 -0300

    add signUp

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Landing from "./pages/Landing";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/SignUp" element={<SignUp />} />
       </Routes>
     </Router>
   );
