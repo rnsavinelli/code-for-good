c113bfed2cb641a2a84b847e813c3ce6eb5c60f1
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-11-12 16:41:42 -0300

    add CORS support

--- server/database.py
+++ server/database.py
@@ -1,6 +1,21 @@
 import sqlite3
 import traceback
 from sqlite3 import Error
+import random
+
+def db_exec(db, query):
+    conn = None
+    try:
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        c.execute(query)
+        conn.commit()
+    except Error as e:
+        traceback.print_exc()
+        print(e)
+    finally:
+        if conn:
+            conn.close()
 
 def get_table(table):
     conn, data, columns = None, None, None
@@ -25,3 +40,26 @@ def get_table(table):
             conn.close()
 
     return data, columns
+
+def write_table(table, dic):
+
+    with open("DATABASE.md","r") as file:
+        db = file.read().strip()
+
+    is_first = True
+
+    values = str()
+    for key in dic.keys():
+        if is_first:
+            values = values + "\"" + dic[key] + "\""
+            is_first = False
+        else:
+            values = values + ",\"" + dic[key] + "\""
+
+    print(values)
+    try:
+        db_exec(db, f'INSERT INTO {conf_table} VALUES ({values})')
+        pass
+    except Error as e:
+        traceback.print_exc()
+        print(e)
--- server/run.py
+++ server/run.py
@@ -1,17 +1,19 @@
 import os
 
 # Import database primitives
-from database import get_table
+from database import get_table, write_table
 
 # Import the framework
 from flask import Flask, g
 from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS
 import markdown
 import urllib
 import json
 
 # Create an instance of Flask
 app = Flask(__name__)
+CORS(app)
 
 # Create the API
 api = Api(app)
@@ -23,16 +25,33 @@ def index():
         return markdown.markdown(content)
 
 class Authenticator(Resource):
+    pass
 
-class UserList(Resource):
+class Users(Resource):
     def get(self):
         entries, columns = get_table('user')
 
         return {'message': 'Success', 'data': {'columns':columns, 'entries':entries}}, 200
 
+    def post(self):
+        parser = reqparse.RequestParser()
+
+        _, columns = get_table('user')
+
+        for col in columns:
+            if str(col) == "id":
+                continue
+            parser.add_argument(str(col), required=True)
+
+        # Parse the arguments into an object
+        args = parser.parse_args()
+
+        write_table('user', args)
+
+        return {'message': 'User registered', 'data': args}, 201
 
 api.add_resource(Authenticator, '/auth')
 
-api.add_resource(UserList, '/users')
+api.add_resource(Users, '/users')
 
 app.run(host='0.0.0.0', port=5000, debug=True)
