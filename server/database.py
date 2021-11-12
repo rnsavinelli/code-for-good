fa9251673cf691a666e440c797782265d63815da
Gonza Castro <gonzacastro99@gmail.com>
2021-11-12 18:20:33 -0300

    login terminado

--- server/database.py
+++ server/database.py
@@ -41,6 +41,37 @@ def get_table(table):
 
     return data, columns
 
+def get_user_by_credentials(table, username, password):
+    conn, data, columns = None, None, None
+
+    with open("DATABASE.md","r") as file:
+        db = file.read().strip()
+
+    try:
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        c.execute(f'SELECT * FROM {table} WHERE username="{username}" AND password="{password}"')
+        data = c.fetchone()
+        if data != None:
+            c.execute(f'SELECT * FROM user WHERE id="{data[1]}"')
+            data2 = c.fetchone()
+            columns = list(map(lambda x: x[0], c.description))
+            conn.commit()
+        else: 
+            return None, None
+
+    except Error as error:
+        print('error xd')
+        traceback.print_exc()
+        print(error)
+        return None, None
+
+    finally:
+        if conn:
+            conn.close()
+
+    return data2, columns
+
 def write_table(table, dic):
 
     with open("DATABASE.md","r") as file:
