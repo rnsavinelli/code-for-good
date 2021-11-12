dc42e9b377d8a2a610b31cd8007f7e35f7efad11
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-11-12 14:03:14 -0300

    unify

--- server/run.py
+++ server/run.py
@@ -1,3 +1,21 @@
-from api import app
+import os
+
+# Import the framework
+from flask import Flask, g
+from flask_restful import Resource, Api, reqparse
+import markdown
+import urllib
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Create the API
+api = Api(app)
+
+@app.route("/")
+def index():
+    with open(os.path.dirname(app.root_path) + '/README.md', 'r') as markdown_file:
+        content = markdown_file.read()
+        return markdown.markdown(content)
 
 app.run(host='0.0.0.0', port=5000, debug=True)
